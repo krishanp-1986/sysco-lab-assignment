6685da5fe3777de47f96440dd9f1fc3e01faadc3
Krishantha Sunil Premaretna <krishantha@backbase.com>
2026-01-09 16:27:35 +0530

    Initial project setup with CocoaPods and Fastlane

---
+++ Syscolabs-star-war-planet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Syscolabs-star-war-planet
+//
+//  Created by Krishantha Sunil Premaretna on 2026-01-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Syscolabs-star-war-planetUITests/Syscolabs_star_war_planetUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Syscolabs_star_war_planetUITestsLaunchTests.swift
+//  Syscolabs-star-war-planetUITests
+//
+//  Created by Krishantha Sunil Premaretna on 2026-01-09.
+//
+
+import XCTest
+
+final class Syscolabs_star_war_planetUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
