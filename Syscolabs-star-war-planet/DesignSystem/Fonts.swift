8872cdb3da2ecd3cb1c693718a876d47d60e5b74
Krishantha Sunil Premaretna <krishantha@backbase.com>
2026-01-09 21:48:37 +0530

    Integrate Cocoapods and restructure project for UIKits

---
+++ Syscolabs-star-war-planet/DesignSystem/Fonts.swift
@@ -0,0 +1,18 @@
+//
+//  Fonts.swift
+//  Syscolabs-star-war-planet
+//
+//  Created by Krishantha Sunil Premaretna on 2026-01-09.
+//
+
+import Foundation
+import UIKit
+
+public extension DesignSystem {
+  struct Fonts {
+    public init() {}
+    public let description = UIFont.systemFont(ofSize: 18)
+    public let header = UIFont.boldSystemFont(ofSize: 24)
+  }
+}
+
